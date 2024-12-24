7963a62b19726a0551a021a43d7bafc56e3ff8cd
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-24 20:51:53 +0800

    add cencel cert

--- src/routes/(user)/cert-requests/+page.svelte
+++ src/routes/(user)/cert-requests/+page.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
   import Table from './components/table/components/table.svelte';
   import { columns } from './components/table/components/columns';
+  import { Skeleton } from '$lib/components/ui/skeleton/index.js';
+  import { initTableState } from './components/table/tableState.svelte';
+  import CancelRequest from './components/cancel-request/cancel-request.svelte';
+
+  const { data } = $props();
+  initTableState();
 </script>
 
 <main class="container min-h-screen py-10">
-  <Table data={[]} {columns} />
+  {#await data.myRequests}
+    <div class="flex flex-col gap-2">
+      <div class="flex justify-end">
+        <div class="flex items-center gap-2.5">
+          <Skeleton class="h-9 w-[300px]" />
+          <Skeleton class="h-9 w-[110px]" />
+        </div>
+      </div>
+
+      <div class="flex justify-end">
+        <Skeleton class="h-9 w-[450px]" />
+      </div>
+
+      <Skeleton class="h-9 w-full" />
+    </div>
+  {:then certificates}
+    <Table data={certificates ?? []} {columns} />
+  {/await}
 </main>
+
+<CancelRequest cancelRequestForm={data.cancelRequestForm} />
