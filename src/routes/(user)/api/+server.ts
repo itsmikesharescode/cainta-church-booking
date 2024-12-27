f200cf15a091f62c0857c78cec53ceaeeb0f007e
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-28 01:51:28 +0800

    try this one

--- src/routes/(user)/api/+server.ts
+++ src/routes/(user)/api/+server.ts
@@ -3,14 +3,12 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 export const POST: RequestHandler = async ({ locals: { supabaseAdmin }, request }) => {
   const body = await request.json();
 
-  if (body.status === 'PAID') {
-    const { error } = await supabaseAdmin.rpc('process_payment', {
-      external_id: body.external_id,
-      xendit_callback: body
-    });
+  const { error } = await supabaseAdmin.rpc('process_payment', {
+    external_id: body.external_id,
+    xendit_callback: body
+  });
 
-    if (error) console.log(error);
-  }
+  if (error) console.log(error);
 
   return json({ status: 'success' });
 };
