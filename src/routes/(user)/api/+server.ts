4f263d36476ead6104b05c94d0ecd8cc30830cba
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-28 01:16:25 +0800

    add checker

--- src/routes/(user)/api/+server.ts
+++ src/routes/(user)/api/+server.ts
@@ -11,10 +11,14 @@ export const POST: RequestHandler = async ({ locals: { supabaseAdmin }, request 
     const churchId = parts[2].split(':')[1] as string;
     const userId = parts[0].split(':')[1] as string;
 
-    if (parts[2].split(':')[0] === 'reservation_id') {
-      reservationId = parts[2].split(':')[1] as string;
-    } else if (parts[2].split(':')[0] === 'cert_id') {
-      certRequestId = parts[2].split(':')[1] as string;
+    if (parts.length > 2) {
+      if (parts[2].split(':')[0] === 'reservation_id') {
+        reservationId = parts[2].split(':')[1] as string;
+      } else if (parts[2].split(':')[0] === 'cert_id') {
+        certRequestId = parts[2].split(':')[1] as string;
+      }
+    } else {
+      console.log('Invalid external_id format:', body.external_id);
     }
 
     const { error } = await supabaseAdmin.rpc('process_payment', {
