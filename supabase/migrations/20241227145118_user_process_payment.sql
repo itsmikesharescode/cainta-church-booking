e8da9f89cbc54d1baaaf77b4b3654c0f7429e7b8
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-28 02:28:08 +0800

    update

--- supabase/migrations/20241227145118_user_process_payment.sql
+++ supabase/migrations/20241227145118_user_process_payment.sql
@@ -5,21 +5,33 @@ create or replace function process_payment(
 declare
     user_id uuid;
     reservation_id numeric;
+    cert_request_id numeric;
     church_id numeric;
+    id_part text;
 begin
-    -- Extract user_id, reservation_id, and church_id from external_id
+    -- Extract user_id and church_id from external_id
     user_id := split_part(external_id, '/', 1)::uuid;
-    reservation_id := split_part(external_id, '/', 2)::numeric;
+    id_part := split_part(external_id, '/', 2);
     church_id := split_part(external_id, '/', 3)::numeric;
 
-    insert into finished_payments_tb (user_id, church_id, reservation_id, cert_request_id, xendit_callback)
-    values (user_id, church_id, reservation_id, cert_request_id, xendit_callback);
+    -- Check if it's a reservation or certificate request
+    if position('res_id=' in id_part) > 0 then
+        reservation_id := replace(id_part, 'res_id=', '')::numeric;
+        cert_request_id := null;
+    elsif position('cert_id=' in id_part) > 0 then
+        cert_request_id := replace(id_part, 'cert_id=', '')::numeric;
+        reservation_id := null;
+    end if;
     
-    if(cert_request_id is not null) then
+    if cert_request_id is not null then
+        insert into finished_payments_tb (user_id, church_id, cert_request_id, xendit_callback)
+        values (user_id, church_id, cert_request_id, xendit_callback);
         update cert_requests_tb
         set status = 'paid'
         where id = cert_request_id;
-    elsif(reservation_id is not null) then
+    elsif reservation_id is not null then
+        insert into finished_payments_tb (user_id, church_id, reservation_id, xendit_callback)
+        values (user_id, church_id, reservation_id, xendit_callback);
         update reservations_tb
         set status = 'paid'
         where id = reservation_id;
