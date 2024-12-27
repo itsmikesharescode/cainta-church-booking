16544ccfd350c9770e080d3abfa04ce8179cc8a1
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-28 01:38:07 +0800

    better debugger

--- src/routes/(user)/api/+server.ts
+++ src/routes/(user)/api/+server.ts
@@ -11,13 +11,13 @@ export const POST: RequestHandler = async ({ locals: { supabaseAdmin }, request 
     const churchId = parts[2].split(':')[1] as string;
     const userId = parts[0].split(':')[1] as string;
 
-    console.log(churchId, userId, reservationId, certRequestId);
-
     if (parts.length > 2) {
       if (parts[2].split(':')[0] === 'reservation_id') {
         reservationId = parts[2].split(':')[1] as string;
+        console.log('reservationId', reservationId);
       } else if (parts[2].split(':')[0] === 'cert_id') {
         certRequestId = parts[2].split(':')[1] as string;
+        console.log('certRequestId', certRequestId);
       }
     } else {
       console.log('Invalid external_id format:', body.external_id);
@@ -26,9 +26,9 @@ export const POST: RequestHandler = async ({ locals: { supabaseAdmin }, request 
     const { error } = await supabaseAdmin.rpc('process_payment', {
       user_id: userId,
       church_id: Number(churchId),
+      xendit_callback: body,
       reservation_id: reservationId ? Number(reservationId) : undefined,
-      cert_request_id: certRequestId ? Number(certRequestId) : undefined,
-      xendit_callback: body
+      cert_request_id: certRequestId ? Number(certRequestId) : undefined
     });
 
     if (error) console.log(error);
