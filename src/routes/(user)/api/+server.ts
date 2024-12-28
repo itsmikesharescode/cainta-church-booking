af942a36d88b658e60356096f296cd6d4ba9227b
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-28 08:37:18 +0800

    test server

--- src/routes/(user)/api/+server.ts
+++ src/routes/(user)/api/+server.ts
@@ -3,12 +3,12 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 export const POST: RequestHandler = async ({ locals: { supabaseAdmin }, request }) => {
   const body = await request.json();
 
-  const { error } = await supabaseAdmin.rpc('process_payment', {
+  const { data, error } = await supabaseAdmin.rpc('process_payment', {
     external_id: body.external_id,
     xendit_callback: body
   });
 
-  if (error) console.log(error);
+  console.log(data, error);
 
   return json({ status: 'success' });
 };
