dc3f48f9ecf7f064cb7724cdf446eb8831fca913
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-28 20:26:03 +0800

    add correct registration

--- src/routes/(index)/+page.server.ts
+++ src/routes/(index)/+page.server.ts
@@ -53,7 +53,7 @@ export const actions: Actions = {
     });
 
     if (error) return fail(401, { msg: error.message });
-    return { form, msg: `Welcome ${data.user?.user_metadata.firstname}!` };
+    return { form, msg: `Please check your email ${form.data.email} for a verification link.` };
   },
 
   forgotPasswordEvent: async ({ locals: { supabase }, request }) => {
